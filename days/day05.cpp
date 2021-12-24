0850504d08ef5d6c2d24a6aa5de4d3c522f60236
Lucas Kim <lucaskamadakim@gmail.com>
2021-12-24 13:05:49 -0500

    Added day 5

---
+++ days/day05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "day05.h"
+
+using namespace std;
+
+string day05(int day) {
+
+    if (day >= 5) {
+		return "Five golden rings\n";
+	} else {
+		return "";
+	}
+
+}
---
+++ days/day05.h
@@ -0,0 +1,8 @@
+#ifndef DAY05_H
+#define DAY05_H
+
+using namespace std;
+
+string day05(int day);
+
+#endif
