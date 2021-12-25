80a458930b4b559418d86c1bc972696ce49c01c7
William <williamzhou2015@gmail.com>
2021-12-24 21:27:11 -0500

    Minor fix

--- days/day12.cpp
+++ days/day12.cpp
@@ -7,19 +7,8 @@ string day12(int day) {
 
     string line = "";
 
-    if(day == 12){
-        line = "12 drummers drumming\n"
-            "Eleven pipers piping\n"
-            "Ten lords a-leaping\n"
-            "Nine ladies dancing\n"
-            "Eight maids a-milking\n"
-            "Seven swans a-swimming\n"
-            "Six geese a-laying\n"
-            "Five golden rings\n"
-            "Four calling birds\n"
-            "Three French hens\n"
-            "Two turtle-doves\n"
-            "And a partridge in a pear tree\n";
+    if(day >= 12){
+        line = "12 drummers drumming\n";
     }
 
     return line;
