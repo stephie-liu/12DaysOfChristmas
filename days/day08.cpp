5f7a6a6b3631b91fc108ddc9abe5e1f638016362
Jessica Sun <jessica.sun@student.tdsb.on.ca>
2021-12-26 17:34:20 -0500

    Day 8 - attempt 1
    Day 8 of 12 days of Christmas Jessica Sun

---
+++ days/day08.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "day08.h"
+
+using namespace std;
+
+string day08(int day){
+
+    if (day >= 8){
+        cout<<"Eight maids a milking \n";
+    } else {
+        cout<<"";
+    }
+
+    return 0;
+
+}
