3380c181e212f1d702b713e53bb43cbd5ad9b076
Nuha-s-0 <nuha.samen@student.tdsb.on.ca>
2021-12-26 23:47:15 -0500

    add day 01

---
+++ days/day01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "day01.h"
+
+using namespace std;
+
+string day01(int day) 
+{
+
+    if (day >= 1) 
+    {return "A partridge in a pear tree\n";
+    }  
+    else
+    {return "";
+    } 
+
+}
---
+++ days/day01.h
@@ -0,0 +1,8 @@
+#ifndef day01_h
+#define day01_h
+
+using namespace std;
+
+string day01(int day);
+
+#endif
