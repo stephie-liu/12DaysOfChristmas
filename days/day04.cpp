3255151a1f9a0feeb41c358f885bbfe5741f9f76
Ryan <ryanfei05@gmail.com>
2021-12-26 23:56:18 -0500

    added days04.cpp and days04.h

---
+++ days/day04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "day04.h"
+
+using namespace std;
+
+string day04(int day){
+
+    string line = "";
+
+    if (day >= 4){
+        line = "Four calling birds\n";
+    } else {
+        line = "";
+    }
+    return line;
+
+}
