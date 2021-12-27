708e133cfeec4661ea6f29b1897c7e47ff0f54e2
aris chen <harryc142857@gmail.com>
2021-12-27 14:50:45 -0500

    added files for ninth day

---
+++ days/day09.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "day09.h"
+
+using namespace std;
+
+string day09(int day){
+    string line = "";
+    if (day >= 9) line = "Nine ladies dancing \n";
+    return line;
+}
---
+++ days/day09.h
@@ -0,0 +1,9 @@
+#ifndef DAY09_H
+#define DAY09_H
+
+using namespace std;
+
+string day09(int day);
+
+#endif
+
